125b67580096c54c06a303ade34510a9682dd0cc
Sanad Marji <74622693+SanadMarji7@users.noreply.github.com>
2022-11-09 15:46:54 +0100

    Update EvalutatingBNN.py

--- Evaluating_BNN/EvalutatingBNN.py
+++ Evaluating_BNN/EvalutatingBNN.py
@@ -31,52 +31,55 @@ totalArea = 0
 
 # BNNForwardProp approximates the total latency, energy, power and area for a given BNN architecture in a forward pass.
 # we devide beta by 64 because the crossbar size we used in the hardware design was 64
-def BNNForwardProp(bnn):
+def BNNForwardProp(alphaBetaDeltaValues, m):
     global totalArea, totalEnergy, totalLatency, E1, L1, A1
     latency = 0
     energy = 0
+    sum_m = 0
+    for i in range(1, m+1):
+        sum_m = sum_m + (0.5**i)
+    sum_m = 1
+    for value in alphaBetaDeltaValues:
+        energy = energy + ((value[0] * ceil(value[1]/64) * value[2]) * sum_m * E1)            
+    totalEnergy = totalEnergy + energy
 
-    for layer in bnn:
-        energy = energy + ((layer[0] * ceil(layer[1]/64) * layer[2]) * E1)
-    totalEnergy = energy
-
-    for layer in bnn:
+    for value in alphaBetaDeltaValues:
         #we devide by 64 because the number of columns in our crossbar is also 64
-        latency = latency + ((ceil(layer[0]/64) * ceil(layer[1]/64) * layer[2]) * L1)
+        latency = latency + (sum_m * (ceil(value[0]/64) * ceil(value[1]/64) * value[2]) * L1)
+
     totalLatency = totalLatency + latency
     totalArea = totalArea + (A1 * 64)
-    print(totalLatency)
-    print(totalEnergy)
-    print(totalArea)
+    print("forward pass latency = ", totalLatency)
+    print("forward pass Energy = ", totalEnergy)
+    print("forward pass Area = ", totalArea)
 
 
 def BNNBackProp(networkArchitecture):
     global total
     global E2, L2, A2, totalEnergy, totalLatency, totalArea
-    totalOperations = 1
-    totalOperations = cnnBackProp(networkArchitecture)
+    bpOperations = cnnBackProp(networkArchitecture)
     latency = 0
     energy = 0
     area = 0
-    
-    energy = totalOperations * E2
+    energy = bpOperations * E2
     totalEnergy = totalEnergy + energy
     
-    latency = totalOperations * L2
+    latency = bpOperations * L2
     totalLatency = totalLatency + latency
 
-    area = totalOperations * A2
+    area = bpOperations * A2
     totalArea = totalArea + area
     
 #Parent function which is meant to be called from the user.
-def evaluateBNNModel(alphaBetaDeltaValues, networkArchitecture):
-    BNNForwardProp(alphaBetaDeltaValues)
+def evaluateBNNModel(alphaBetaDeltaValues, networkArchitecture, m):
+    BNNForwardProp(alphaBetaDeltaValues, m)
     BNNBackProp(networkArchitecture)
 
 ###########################################################################################################################################
-##############################     input BNN architecture here     ##############################
-##   0 element: Array of Arrays, where each index array contains alpha, beta, delta values     ##
+###########################     inputs for evaluateBNNModel(0, 1)   #############################
+##   0 element: Array of Arrays, every array contains alpha, beta, delta values of each layer  ##
 ##   1 element: CNN model architecture used                                                    ##
+##   2 element: value of m                                                    ##
 #################################################################################################
 
 VGG3BNNValues = [[64, 576, 196], [2048, 3136, 1]]
@@ -85,9 +88,8 @@ VGG7BNNValues = [[128, 1152, 1024], [256, 1152, 256], [256, 2304, 256], [512, 23
 VGG3 = [[28, 28, 1], [True, False, True, False], [64, 0, 64, 0], 1, 1, [3, 3, 1], [2,2], 2, 1, 1, [64, 2048, 10]]
 VGG7 = [[32, 32, 3], [True, True, False, True, True, False, True, True, False], [128, 128, 0, 256, 256, 0, 512, 512, 0], 1, 1, [3, 3, 3], [2,2], 2, 1, 1, [512, 1024, 10]]
 
-
-#evaluateBNNModel(VGG3BNNValues, VGG3)
-evaluateBNNModel(VGG7BNNValues, VGG7)
+evaluateBNNModel(VGG3BNNValues, VGG3, 6)
+#evaluateBNNModel(VGG7BNNValues, VGG7, 6)
 
 print("total latency of model := ", totalLatency)
 print("total Energy of model := ", totalEnergy)
